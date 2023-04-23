9af47adf8233193125917db23fb2ab1bbc52bcfd
zhangyu <1802833394@qq.com>
2023-04-23 18:21:33 +0800

    spring的启动和扫描逻辑的模拟实现

--- src/main/java/com/spring/OpenallzzzApplicationContext.java
+++ src/main/java/com/spring/OpenallzzzApplicationContext.java
@@ -1,5 +1,10 @@
 package com.spring;
 
+import com.sun.javaws.jnl.LibraryDesc;
+
+import java.io.File;
+import java.net.URL;
+
 public class OpenallzzzApplicationContext { // 容器类
 
     private final Class<?> configClass;
@@ -9,6 +14,42 @@ public class OpenallzzzApplicationContext { // 容器类
 
         // 解析配置类
         // ComponentScan注解 --> 扫描路径 -- > 扫描
+
+        ComponentScan componentScanAnnotation = configClass.getDeclaredAnnotation(ComponentScan.class);
+        String path = componentScanAnnotation.value(); // 获取注解上的类路径
+
+        path = path.replace('.', '/'); // 替换成文件分割符
+
+        // 扫描
+        /*类加载器
+            Bootstrap-->jre/lib
+            Ext-->jre/ext/lib
+            App-->classpath
+        */
+
+        // 获取类加载器
+        ClassLoader classLoader = OpenallzzzApplicationContext.class.getClassLoader();
+        URL resource = classLoader.getResource(path);
+        File file = new File(resource.getFile());
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                String fileName = f.getAbsolutePath();
+                if(fileName.endsWith(".class")) { // 判断是否是类文件
+                    String className = fileName.substring(fileName.indexOf("com"), fileName.indexOf(".class"));
+                    className = className.replace('\\', '.');
+                    Class<?> aClass = null;
+                    try {
+                        aClass = classLoader.loadClass(className);
+                        if (aClass.isAnnotationPresent(Component.class)) { // 当前类是一个Bean
+
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.getStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     public Object getBean(String beanName) {
